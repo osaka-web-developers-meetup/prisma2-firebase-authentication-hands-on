4e6b3e8768e9d211a4b57089f51e2b5c15698ee1
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2019-06-29 05:00:25 +0900

    Fix firebaseAdmin initialize

--- backend/src/client/firebase.ts
+++ backend/src/client/firebase.ts
@@ -8,9 +8,9 @@ export interface User {
 const admin = firebaseAdmin.initializeApp(
   {
     credential: firebaseAdmin.credential.cert({
-      projectId: process.env.GCP_PROJECT_ID,
+      projectId: process.env.PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n")
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL
   },
